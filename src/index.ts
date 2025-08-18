2aee8c76eadfab8e632565338f4e57569bb288ef
Yasin Silavi <ys.silavi@gmail.com>
2025-08-18 17:44:31 +0330

    bug: ignore decimal numbers

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 import plugin from "tailwindcss/plugin";
 
-const safeAreaPlugin = plugin(({ addUtilities, theme }) => {
-  const spacing = theme("spacing");
+const safeAreaPlugin: ReturnType<typeof plugin> = plugin(({ addUtilities, theme }) => {
+  const spacing = theme("spacing") || {};
 
-  if (!spacing) {
-    console.warn("[Tailwind Safe Areas] You have to add a configuration for spacing in your project");
-    return;
+  if (Object.keys(spacing).length === 0) {
+    console.warn("[Tailwind Safe Areas] Spacing configuration is empty.");
   }
 
   const entries = Object.entries(spacing).flatMap(([key, value]) => {
-    // NOTE: Escape the dot so 0.5 → 0\.5
-    const escaped = key.replace(/\./g, "\\.");
+    // NOTE: Escape decimal numbers → 0.5
+    const hasDecimal = key.includes(".");
+
+    if (hasDecimal) return []
 
     return [
-      [`.mt-safe-${escaped}`, { marginTop: `calc(${value} + env(safe-area-inset-top))` }],
-      [`.mb-safe-${escaped}`, { marginBottom: `calc(${value} + env(safe-area-inset-bottom))` }],
-      [`.pt-safe-${escaped}`, { paddingTop: `calc(${value} + env(safe-area-inset-top))` }],
-      [`.pb-safe-${escaped}`, { paddingBottom: `calc(${value} + env(safe-area-inset-bottom))` }],
-      [`.top-safe-${escaped}`, { top: `calc(${value} + env(safe-area-inset-top))` }],
-      [`.bottom-safe-${escaped}`, { bottom: `calc(${value} + env(safe-area-inset-bottom))` }],
+      [`.mt-safe-${key}`, { marginTop: `calc(${value} + env(safe-area-inset-top))` }],
+      [`.mb-safe-${key}`, { marginBottom: `calc(${value} + env(safe-area-inset-bottom))` }],
+      [`.pt-safe-${key}`, { paddingTop: `calc(${value} + env(safe-area-inset-top))` }],
+      [`.pb-safe-${key}`, { paddingBottom: `calc(${value} + env(safe-area-inset-bottom))` }],
+      [`.top-safe-${key}`, { top: `calc(${value} + env(safe-area-inset-top))` }],
+      [`.bottom-safe-${key}`, { bottom: `calc(${value} + env(safe-area-inset-bottom))` }],
     ];
   });
 
   addUtilities(Object.fromEntries(entries));
-}) as any;
+});
 
 export default safeAreaPlugin;
